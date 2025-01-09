ce646f336394ee115455edbefe4d4f562a291f6f
OSCAR ARJONES SANCHEZ <oarjones@gmail.com>
2025-01-09 12:17:01 +0100

    Fase 2: Creada clase temporal GameEventsChannelSO para evitar errores de compilación.

---
+++ Assets/Scripts/Application/EventChannels/GameEventsChannelSO.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace MagicWords.Application.EventChannels
+{
+    [CreateAssetMenu(menuName = "Events/Game Events Channel")]
+    public class GameEventsChannelSO : ScriptableObject
+    {
+        // Dejaremos este script vacío por ahora. Lo completaremos en la Fase 5.
+    }
+}
